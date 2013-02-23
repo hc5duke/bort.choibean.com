195e0a388a94f9ea03ee61b6d7f5cccf0e761354
hc5duke <hc5duke@gmail.com>
2013-02-23 15:53:47 -0800

    use yui compressor

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
-coffee -p -c bart.coffee | sed '1 c\' | perl -pe 's/^ +|\n//g' > bart.js
+coffee -p -c bart.coffee | sed '1 c\' > bart.js
+yuicompressor -o bart.js bart.js
 sass bart.scss -t compressed > bart.css
 haml bart.haml | perl -pe 's/^ +|\n+/ /g' | perl -pe 's/ +/ /g' | perl -pe 's/> </></g' | perl -pe 's/ $//g'> out/bart.html
-# haml bart.haml | perl -pe 's/^ +//g' | perl -pe 's/ +/ /g' | perl -pe 's/> </></g' | perl -pe 's/ $//g'> out/bart.html
 rm bart.js bart.css
