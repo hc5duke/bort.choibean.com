99d0dfe0d1756ee0530f938ffb26c3dc9903e407
hc5duke <hc5duke@gmail.com>
2013-02-23 15:41:12 -0800

    add more head

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,5 @@
-coffee -p -c bart.coffee | sed '1 c\' | perl -pe 's/^ +|\n//g' > out/bart.js
+coffee -p -c bart.coffee | sed '1 c\' | perl -pe 's/^ +|\n//g' > bart.js
+sass bart.scss -t compressed > bart.css
 haml bart.haml | perl -pe 's/^ +|\n+/ /g' | perl -pe 's/ +/ /g' | perl -pe 's/> </></g' | perl -pe 's/ $//g'> out/bart.html
 # haml bart.haml | perl -pe 's/^ +//g' | perl -pe 's/ +/ /g' | perl -pe 's/> </></g' | perl -pe 's/ $//g'> out/bart.html
+rm bart.js bart.css
