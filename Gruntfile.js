911525fdadb323fc31b8b18ee7e1558c1dac52cb
Hwan-Joon Choi <hc5duke@gmail.com>
2014-09-11 23:47:53 -0700

    gruntify everything

---
+++ Gruntfile.js
@@ -0,0 +1,114 @@
+(function(){
+  module.exports = function(grunt) {
+    grunt.initConfig({
+      concurrent: {
+        default: {
+          tasks: [
+            'nodemon', // express server on dist
+            'watch'    // watch and compile code
+          ],
+          options: { logConcurrentOutput: true }
+        },
+      },
+
+      aws: grunt.file.readJSON('.grunt-aws.json'),
+
+      s3: {
+        options: {
+          key: '<%= aws.key %>',
+          secret: '<%= aws.secret %>',
+          bucket: '<%= aws.bucket %>',
+          access: 'public-read',
+          region: 'us-west-1',
+          headers: {
+            // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+            "Cache-Control": "max-age=630720000, public",
+            "Expires": new Date(Date.now() + 63072000000).toUTCString()
+          }
+        },
+        default: {
+          upload: [
+            { src: 'dist/index.html', dest: '/index.html' }
+          ]
+        }
+      },
+
+      haml: {
+        dist: {
+          files: { 'dist/index.html': 'src/index.haml' }
+        }
+      },
+
+      coffee: {
+        compile: {
+          files: { 'tmp/bart.js': ['src/bart.coffee'] }
+        }
+      },
+
+      sass: {
+        dist: {
+          options: { style: 'expanded' },
+          files: { 'tmp/bart.css': 'src/bart.scss' }
+        }
+      },
+
+      uglify: {
+        my_target: {
+          files: { 'tmp/bart.min.js': ['tmp/bart.js'] }
+        }
+      },
+
+      cssmin: {
+        combine: {
+          files: { "tmp/bart.min.css": ["tmp/bart.css"] }
+        }
+      },
+
+      nodemon: {
+        dev: {
+          script: 'server.js',
+          options: {
+            verbose: false
+          },
+          ignore: ['*'] // don't need to restart server
+        }
+      },
+
+      watch: {
+        files: ['src/*'],
+        tasks: ['prep-dist-dir']
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-haml');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-s3');
+
+    // launch time bootstrap tasks
+    grunt.registerTask('deploy', [ 'prep-dist-dir', 's3' ]);
+
+    grunt.registerTask('prep-dist-dir', [
+      'coffee',
+      'sass',
+      'uglify', // minify FE javascript
+      'cssmin', // concat FE css
+      'haml'    // needs access to minified files
+    ]);
+
+    grunt.registerTask('default', [
+
+      // compile frontend source and prep dist dir
+      'prep-dist-dir',
+
+      // run all concurrent watchers
+      'concurrent:default'
+
+    ]);
+  };
+}());
---
+++ server.js
@@ -0,0 +1,11 @@
+(function() {
+  "use strict";
+  // Express server setup
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname + '/dist'));
+
+  // server config
+  var port = process.env.PORT || 3111;
+  var server = app.listen(port);
+})();
